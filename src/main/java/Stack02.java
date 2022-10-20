4550f229f9d97656f168be93af6a0a66dd46ccf3
Miji <mjkim2304@gmail.com>
2022-10-20 09:40:44 +0900

    push

---
+++ src/main/java/Stack02.java
@@ -0,0 +1,19 @@
+
+public class Stack02 {
+    private Integer[] arr;
+    private int top=0;
+    public Stack02(){ arr=new Integer[10000]; }
+    public Stack02(int size){
+        this.arr=new Integer[size];
+    }
+    public void push(int value){
+        arr[top++]=value;
+    }
+    public Integer[] getArr(){
+        return arr;
+    }
+
+    public int pop(){
+        return arr[--top];
+    }
+}
---
+++ src/test/java/Stack02Test.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Stack02Test {
+    @BeforeEach
+    void setUp() {
+        System.out.println("before each");
+    }
+    @Test
+    @DisplayName("push가 잘되는지")
+    void push() {
+        Stack02 st = new Stack02();
+        st.push(10);
+        st.push(20);
+        Integer[] arr=st.getArr();
+        assertEquals(20, arr[0]);
+        assertEquals(10, arr[1]);
+    }
+    @Test
+    void pushAndPop() {
+        Stack02 st = new Stack02();
+        st.push(10);
+        st.push(20);
+        assertEquals(20, st.pop());
+        assertEquals(10, st.pop());
+        st.push(30);
+        assertEquals(30, st.pop());
+    }
+}
