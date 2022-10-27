7c76aa42212fa9dd08a7c91d0264c23a4a9a43a7
Miji <mjkim2304@gmail.com>
2022-10-27 10:31:33 +0900

    프로그래머스 완주하지 못한 선수: 동명이인 문제 해결

--- src/main/java/ProgrammersHash.java
+++ src/main/java/ProgrammersHash.java
@@ -9,25 +9,35 @@ public class ProgrammersHash {
     * key가 0 (completion에 없는 참가자) 이면 그 사람 이름 return
     *
     *ERROR: 동명이인이 있고, 한사람이 완주했을 때 무조건 완주한 사람으로 처리됨
+    *REFACTOR: allParticipants의 value를 count로 사용.
     * */
 
-    public String solution(String[] participant, String[] completion) {
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+
+        System.out.println(solution(participant, completion));
+    }
+    public static String solution(String[] participant, String[] completion) {
         Map<String, Integer> allParticipants = new HashMap<>();
 
         // 참가자 이름 모두 저장
         for (int i = 0; i < participant.length; i++) {
             String key = participant[i];
-            allParticipants.put(key, 0);
+            if (!allParticipants.containsKey(key)){
+                allParticipants.put(key, 0);
+            }
+            allParticipants.put(key, allParticipants.get(key) + 1);
         }
 
         // 완주한 이름 모두 value 고치기
         for (int i = 0; i < completion.length ; i++) {
             String key = completion[i];
-            allParticipants.put(key, 1);
+            allParticipants.put(key, allParticipants.get(key) - 1);
         }
 
         for (String key: allParticipants.keySet()){
-            if(allParticipants.get(key) == 0){
+            if(allParticipants.get(key) > 0){
                 return key;
             }
         }
