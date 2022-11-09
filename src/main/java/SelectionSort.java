092f78881e333dfb429b8ce02d7bfa89a6a772dd
Miji <mjkim2304@gmail.com>
2022-11-09 10:47:54 +0900

    Selection Sort using interface and template callback

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,10 +1,31 @@
 import java.util.Arrays;
 
+interface SortStrategy {
+    boolean apply(int a, int b);
+}
+
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
         System.out.println(Arrays.toString(ascending(arr)));
+        System.out.println(Arrays.toString(selectionSort(arr, (a, b) -> a > b)));
         System.out.println(Arrays.toString(descending(arr)));
+        System.out.println(Arrays.toString(selectionSort(arr, (a, b) -> a < b)));
+    }
+
+    public static int[]selectionSort(int[] arr, SortStrategy stmt){
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < arr.length - 1; j++) {
+                if (stmt.apply(arr[minIndex], arr[j])) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        return arr;
     }
     
     public static int[] ascending(int[] arr) {
