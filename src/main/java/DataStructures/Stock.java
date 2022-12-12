1813b314c4c4113072fa6bef4aa071d2dc53fa55
u <mjkim2304@gmail.com>
2022-12-12 17:38:32 +0900

    프로그래머스 주식가격 (cleaner code)

--- src/main/java/DataStructures/Stock.java
+++ src/main/java/DataStructures/Stock.java
@@ -8,7 +8,23 @@ public class Stock {
         int[] answer = secondsUntilDrop(prices);
         System.out.println(Arrays.toString(answer));
     }
+    // 내 코드
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < answer.length - 1; i++) {
+            int count = 0;
+            for (int j = i + 1; j < answer.length; j++) {
+                count++;
+                if (prices[j] < prices[i]) {
+                    break;
+                }
+            }
+            answer[i] = count;
+        }
+        return answer;
+    }
 
+    // 더 깔끔한 코드
     public static int[] secondsUntilDrop(int[] prices) {
         int length = prices.length;
         int[] answer = new int[length];
