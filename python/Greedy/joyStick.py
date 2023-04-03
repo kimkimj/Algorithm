077ca788ad7f915469db79be700e2861eea8aebf
u <mjkim2304@gmail.com>
2023-04-03 11:58:44 +0900

    [프로그래머스] 조이스틱 - INCOMPLETE (37/100)

--- python/Greedy/joyStick.py
+++ python/Greedy/joyStick.py
@@ -1,11 +1,20 @@
 def solution(name):
     answer = 0
     a_count = 0
+
     for i in range(len(name)):
         count = ord(name[i])
-        answer += min(count - 65, 95 - count + 1)
-        if name[i] == 'A':
+        k = min(count - 65, 90 - count + 1)
+        answer += k
+
+    next_a_index = name.find('A')
+    if next_a_index != -1:
+        while name[next_a_index] == 'A':
             a_count += 1
+            next_a_index += 1
+    if a_count == 1 and name.find('A') == 0:
+        a_count = 0
+    answer += len(name) -1 - a_count
 
     print(answer)
 
