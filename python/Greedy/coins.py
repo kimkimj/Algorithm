d22c2bc2ddc80273295eb1ca680b90d7cf403f22
u <mjkim2304@gmail.com>
2023-04-04 11:13:08 +0900

    [백준] 동전 0 reversed for loop을 이용해 모든 동전들을 total에 나누고 그 나머지를 계속해서 더 작은 동전들로 나눈다

--- python/Greedy/coins.py
+++ python/Greedy/coins.py
@@ -4,13 +4,8 @@ for i in range(n):
     coins.append(int(input()))
 
 count = 0
-temp_total = total
-while temp_total > 0:
-    largest_coin = coins[0]
-    i = 0
-    while temp_total > coins[i]:
-        largest_coin = coins[i]
-        i += 1
-    count += temp_total // largest_coin
-    temp_total = temp_total % largest_coin
+for i in reversed(range(n)):
+    count += total // coins[i]
+    total = total % coins[i]
+
 print(count)
