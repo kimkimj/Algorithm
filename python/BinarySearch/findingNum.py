22cf922be26b4c2c93cd19adc3f859345a67fd33
u <mjkim2304@gmail.com>
2023-04-07 11:44:12 +0900

    [백준]수 찾기 - 복습하기

--- python/BinarySearch/findingNum.py
+++ python/BinarySearch/findingNum.py
@@ -1,24 +1,29 @@
 
-n = 5
-given = [4, 1, 5, 2, 3]
+n = int(input())
+#n_arr = [4, 1, 5, 2, 3]
 #n = int(input())
-#given = list(map(int, input().split()))
-m = 5
-b = [1, 3, 7, 9, 5]
-
-given.sort()
-for num in b:
-    mid = len(given) // 2
+n_arr = list(map(int, input().split()))
+#m = 5
+m = int(input())
+#m_arr = [1, 3, 7, 9, 5]
+m_arr = list(map(int, input().split()))
+n_arr.sort()
+for num in m_arr:
     found = False
-    while mid >= 0 and mid < len(given):
-        if num == given[mid]:
+    start = 0
+    end = n - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if num == n_arr[mid]:
             found = True
             break
-        elif num < given[mid]:
-            mid //= 2
-        else:
-            mid = (len(given) + mid) // 2
-    if found == False:
+        elif num < n_arr[mid]:
+            end = mid - 1
+        elif num > n_arr[mid]:
+            start = mid + 1
+
+    if not found:
         print(0)
     else:
         print(1)
