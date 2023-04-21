135f11f50ef931c644991c8220b8ac3db1cd70c4
u <mjkim2304@gmail.com>
2023-04-21 12:01:23 +0900

    [백준] 구간 합 구하기 4 Refactored (need to review)

--- python/etc/cumulativeSum.py
+++ python/etc/cumulativeSum.py
@@ -1,19 +1,16 @@
+import sys
+
+input = sys.stdin.readline
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-sums = [0] * n
-sums[0] = arr[0]
-for i in range(1, n):
-    sums[i] += arr[i] + sums[i - 1]
+# 맨 앞에 0을 넣어 인덱스를 맞춰준다
+sums = [0]
+total = 0
+for i in range(n):
+    total += arr[i]
+    sums.append(total)
 
-answer = [0] * m
-
-for a in range(m):
+for _ in range(m):
     i, j = map(int, input().split())
-    if i > 1:
-        answer[a] = sums[j - 1] - sums[i - 2]
-    else:
-        answer[a] = sums[j - 1]
-
-for i in range(m):
-    print(answer[i])
+    print(sums[j] - sums[i - 1])
