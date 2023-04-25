3a9b70c600ec53e3e2ccdb3cea260014d81a02ce
u <mjkim2304@gmail.com>
2023-04-25 10:28:51 +0900

    [백준] 하노이 탑 이동 순서 (복습)

--- python/Recursion/towerOfHanoi.py
+++ python/Recursion/towerOfHanoi.py
@@ -1,11 +1,19 @@
 def hanoi(num, start, end):
+
     if num == 1:
         print(start, end)
     else:
+        # 1) num - 1개를 auxiliary rod로 이동
         hanoi(num - 1, start, 6 - start - end)
+
+        # 2) 가장 큰 원반을 목표 rod로 이동
         print(start, end)
+
+        #3) num - 1개를 auxilary rod에서 목표 rod로 이동
         hanoi(num - 1, 6 - start - end, end)
 
-num = int(input())
-print(2 ** num - 1)
-hanoi(num, 1, 3)
+n = int(input)
+print(2 ** n - 1)
+hanoi(n, 1, 3)
+
+#https://brenden.tistory.com/31
