2add08b8d713b24e32021871e571367155da1033
u <mjkim2304@gmail.com>
2023-04-27 19:34:36 +0900

    [백준] 단지 번호 붙이기 (bfs) *

--- python/BFS-DFS/neighboringApartments.py
+++ python/BFS-DFS/neighboringApartments.py
@@ -7,45 +7,46 @@ homes = [list(map(int, list(input().strip()))) for _ in range(n)]
 counts = [0]
 visited = [[0] * n for _ in range(n)]
 
-def bfs():
-    vil_num = 1
-    queue = deque()
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    # (-1, 0) left, (1, 0) right, (0, -1) above, (0, 1) below
+queue = deque()
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+# (-1, 0) left, (1, 0) right, (0, -1) above, (0, 1) below
 
-    for i in range(n):
-        for j in range(n):
-            # if the apartment has not been visited
-            if homes[i][j] != 0 and visited[i][j] == 0:
-                cnt = 0
-                queue.append([i][j])
-                visited[i][j] = vil_num
-                while queue:
-                    x, y = queue.popleft()
+for i in range(n):
+    for j in range(n):
+        # if the apartment exists, but has not been visited
+        if homes[i][j] == 1 and visited[i][j] == 0:
+            cnt = 1
+            queue.append([i, j])
+            visited[i][j] = 1
+            while queue:
+                x, y = queue.popleft()
 
-                    for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
+                # explore all four adjacent cells
+                for k in range(4):
+                    nx = x + dx[k]
+                    ny = y + dy[k]
 
-                        # if the neighboring cell has not been visited and
-                        # there is an apartment at the location
-                        if 0 <= nx < n and 0 <= ny < n and \
-                                visited[nx][ny] == 0 and homes[nx][ny] != 0:
+                    # if the neighboring cell has not been visited and
+                    # there IS an apartment at the location
+                    if 0 <= nx < n and 0 <= ny < n and \
+                             homes[nx][ny] != 0 and visited[nx][ny] == 0:
 
-                            # the neighboring apartment has the same vil_num as the current
-                            visited[nx][ny] = vil_num
-                            queue.append([nx, ny])
-                            cnt += 1
+                        # mark the apartment as visited
+                        # add it to the queue
+                        # add to the count
+                        visited[nx][ny] = 1
+                        queue.append([nx, ny])
+                        cnt += 1
 
-                    counts.append(cnt)
-                    vil_num += 1
-    return vil_num - 1
+            # when the queue ends, bfs has ended
+            # no more connected cells
+            counts.append(cnt)
 
-vil_num = bfs()
+print(len(counts) - 1)
 counts.sort()
-print(vil_num)
-print(*counts[1:])
+print(*counts[1:], sep='\n')
+
 
 
 
