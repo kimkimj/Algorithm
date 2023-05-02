664efc155c9732c1d9be003021100240207c1469
u <mjkim2304@gmail.com>
2023-05-02 09:42:00 +0900

    [백준] 숨바꼭질 * - array의 사이즈를 n과 k의 max 값으로 바꾸니 성공 - for i in (index + 1, index - 1, index * 2):

--- python/BFS-DFS/hide_and_seek.py
+++ python/BFS-DFS/hide_and_seek.py
@@ -1,18 +1,21 @@
 from collections import deque
 
 n, k = map(int, input().split())
-graph = [-1] * max(n + 1, k + 1)
+#graph = [-1] * max(n + 1, k + 1)
+graph = [-1] * 100001
 queue = deque([n])
 graph[n] = 0
 while queue:
     index = queue.popleft()
-    delta = [-1, 1, index]
-    for i in range(3):
-        nx = index + delta[i]
+    #delta = [-1, 1, index]
+    #for i in range(3):
+    #    nx = index + delta[i]
+    for i in (index + 1, index - 1, index * 2):
         if 0 <= nx < len(graph) and graph[nx] == -1:
-            nx = index + delta[i]
+            #nx = index + delta[i]
+            nx = index + i
             graph[nx] = graph[index] + 1
-            if nx == n:
+            if nx == k:
                 break
             queue.append(nx)
 
