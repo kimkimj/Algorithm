4e512b9bb99bb64cd260e72825a5715db74554f8
u <mjkim2304@gmail.com>
2023-05-04 10:13:15 +0900

    [백준] 벽 부수고 이동하기 (실패: 시간초과)

--- python/BFS-DFS/breaking_wall.py
+++ python/BFS-DFS/breaking_wall.py
@@ -6,7 +6,6 @@ def bfs(wall_r, wall_c, maze):
     queue = deque([(0, 0)])
     visited = [[-1] * columns for _ in range(rows)]
     visited[0][0] = 1
-    count = 1
 
     while queue:
         r, c = queue.popleft()
@@ -16,13 +15,17 @@ def bfs(wall_r, wall_c, maze):
             nc = c + dc
 
             if 0 <= nr < rows and 0 <= nc < columns \
-                and maze[nr][nc] == 0 and visited == -1:
-                visited[nr][nc] == 0
+                and maze[nr][nc] == 0 and visited[nr][nc] == -1:
+
                 queue.append((nr, nc))
-                count += 1
-                if nr == rows - 1 and nc == columns - 1:
-                    return count
-    return -1
+                # count += 1 이렇게 하면 방문한 모든 vertices를 count하게 된다
+                # 방문한 level을 count해야 하기 때문에
+                # count = maze[r][c] + 1 이렇게하면 0과 1 하나에 1을 더한 결과를 반환
+                visited[nr][nc] = visited[r][c] + 1
+    #the function did not make a copy of the 2d array, but referenced to it
+    # the change made by the function applies to the original reference
+    maze[wall_r][wall_c] = 1
+    return visited[rows - 1][columns - 1]
 
 rows, columns = map(int, input().split())
 maze = [list(map(int, list(input()))) for _ in range(rows)]
@@ -39,3 +42,7 @@ if shortest == columns * rows:
     print(-1)
 else:
     print(shortest)
+
+# too many syntax errors that do not result in exceptions, but unexpected results
+# visited = ... instead of visted[][]
+# visted[][] == 1 instead of visited[][] = ..
