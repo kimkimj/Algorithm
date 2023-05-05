7b83dcd20d291e04a11d6a984830a3fd78860484
u <mjkim2304@gmail.com>
2023-05-05 10:28:11 +0900

    [백준] 단지번호붙이기

--- python/BFS-DFS/neighboringApartments.py
+++ python/BFS-DFS/neighboringApartments.py
@@ -1,54 +1,34 @@
-from sys import stdin
 from collections import deque
 
-input = stdin.readline
+
 n = int(input())
-homes = [list(map(int, list(input().strip()))) for _ in range(n)]
-counts = [0]
-visited = [[0] * n for _ in range(n)]
+apt = [list(map(int, list(input()))) for _ in range(n)]
+delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-queue = deque()
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-# (-1, 0) left, (1, 0) right, (0, -1) above, (0, 1) below
+answer = []
 
-for i in range(n):
-    for j in range(n):
-        # if the apartment exists, but has not been visited
-        if homes[i][j] == 1 and visited[i][j] == 0:
-            cnt = 1
-            queue.append([i, j])
-            visited[i][j] = 1
+queue = deque([(0, 0)])
+for r in range(n):
+    for c in range(n):
+        if apt[r][c] == 1:
+            count = 1
+            queue.append((r, c))
+            apt[r][c] = 0
+
             while queue:
-                x, y = queue.popleft()
+                y, x = queue.popleft()
 
-                # explore all four adjacent cells
-                for k in range(4):
-                    nx = x + dx[k]
-                    ny = y + dy[k]
+                for dx, dy in delta:
+                    ny = y + dy
+                    nx = x + dx
 
-                    # if the neighboring cell has not been visited and
-                    # there IS an apartment at the location
                     if 0 <= nx < n and 0 <= ny < n and \
-                             homes[nx][ny] != 0 and visited[nx][ny] == 0:
+                        apt[ny][nx] == 1:
+                        queue.append((ny, nx))
+                        apt[ny][nx] = 0
+                        count += 1
 
-                        # mark the apartment as visited
-                        # add it to the queue
-                        # add to the count
-                        visited[nx][ny] = 1
-                        queue.append([nx, ny])
-                        cnt += 1
-
-            # when the queue ends, bfs has ended
-            # no more connected cells
-            counts.append(cnt)
-
-print(len(counts) - 1)
-counts.sort()
-print(*counts[1:], sep='\n')
-
-
-
-
-
-
+            answer.append(count)
+print(len(answer))
+answer.sort()
+print(*answer, sep="\n")
