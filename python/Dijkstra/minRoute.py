f10987b32751d7c8fb69f1ede904b11dea509d22
u <mjkim2304@gmail.com>
2023-05-16 10:27:21 +0900

    [백준] 최단경로 (복습) *

--- python/Dijkstra/minRoute.py
+++ python/Dijkstra/minRoute.py
@@ -1,33 +1,29 @@
 import sys
 import heapq
 
-input = sys.stdin.readline
 INF = float('inf')
+input = sys.stdin.readline
 
 V, E = map(int, input().split())
 start = int(input())
-
-# 비용을 무한대로 초기화
-min_dist = [INF] * (V + 1)
-
 graph = [[] for _ in range(V + 1)]
-for _ in range(E):
+for _ in range(V):
     a, b, w = map(int, input().split())
     graph[a].append((b, w))
 
-que = []
-heapq.heappush(que, (0, start)) #minimum distance to the node, node
-# 시작 node의 비용은 0
+min_dist = [INF] * (V + 1)
+que = [(0, start)] # weight determines the priority
 min_dist[start] = 0
 while que:
     current_weight, current = heapq.heappop(que)
 
     for adjacent, adjacent_weight in graph[current]:
-        new_w = current_weight + adjacent_weight
 
-        if new_w < min_dist[adjacent]:
-            min_dist[adjacent] = new_w
-            heapq.heappush(que, (new_w, adjacent))
+        # check if the distance to the neighboring node through this node is shorter
+        new_weight = current_weight + adjacent_weight
+        if new_weight < min_dist[adjacent]:
+            min_dist[adjacent] = new_weight
+            que.append((new_weight, adjacent))
 
 for i in range(1, V + 1):
     if min_dist[i] == INF:
