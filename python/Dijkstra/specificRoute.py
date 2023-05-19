b7e5fa63a7189a9f94e63e2e009e50fc9c734f87
u <mjkim2304@gmail.com>
2023-05-19 10:41:18 +0900

    [백준] 특정한 최단 경로 (복습) *

--- python/Dijkstra/specificRoute.py
+++ python/Dijkstra/specificRoute.py
@@ -3,39 +3,38 @@ import heapq
 
 input = sys.stdin.readline
 INF = float('inf')
-
 V, E = map(int, input().split())
 graph = [[] for _ in range(V + 1)]
 for _ in range(E):
     a, b, w = map(int, input().split())
-    graph[a].append((b, w))
-    graph[b].append((a, w))
+    graph[a].append((w, b))
+    graph[b].append((w, a))
 v1, v2 = map(int, input().split())
 
-# Start -> V1 -> V2 -> END
-# START -> V2 -> V1 -> END
-def dijkstra(start):
-    que = [(0, start)]
+def dijakstra(start):
     min_dist = [INF] * (V + 1)
     min_dist[start] = 0
-    while que:
-        current_weight, current = heapq.heappop(que)
+    queue = [(0, start)]
+    while queue:
+        current_dist, current = heapq.heappop(queue)
 
-        for adjacent, adjacent_weight in graph[current]:
-            new_weight = current_weight + adjacent_weight
-            if new_weight < min_dist[adjacent]:
-                min_dist[adjacent] = new_weight
-                que.append((new_weight, adjacent))
+        for distance_to_adjacent, adjacent in graph[current]:
+            new_dist = current_dist + distance_to_adjacent
+
+            if new_dist < min_dist[adjacent]:
+                min_dist[adjacent] = new_dist
+                heapq.heappush(queue, (new_dist, adjacent))
     return min_dist
 
-one = dijkstra(1)
-v1 = dijkstra(v1)
-v2 = dijkstra(v2)
+one = dijakstra(1)
+v_one = dijakstra(v1)
+v_two = dijakstra(v2)
 
-answer = min(one[v1] + v1[v2] + v2[V], one[v2] + v2[v1] + v1[V])
+path1 = one[v1] + v_one[v2] + v_two[V]
+path2 = one[v2] + v_two[v1] + v_one[V]
+answer = min(path1, path2)
 
-if answer < INF:
-    print(answer)
-else:
+if answer == INF:
     print(-1)
-
+else:
+    print(answer)
