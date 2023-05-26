cf9df9842386e83c0391e64def0122a216deaae5
u <mjkim2304@gmail.com>
2023-05-26 11:35:22 +0900

    [프로그래머스] 숫자 문자열과 영단어 *

--- python/programmersLvl1/words_to_num.py
+++ python/programmersLvl1/words_to_num.py
@@ -1,18 +1,10 @@
 def solution(s):
-    dict = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4,
-            'five':5, 'six':6, 'seven':7,'eight':8, 'nine':9}
+    dict = {'zero':'0', 'one': '1', 'two': '2', 'three':'3', 'four':'4',
+            'five':'5', 'six':'6', 'seven':'7','eight':'8', 'nine':'9'}
 
-    num = ""
-    word = ""
-    for i in range(len(s)):
-        if 'a' <= s[i] <'z':
-            word += s[i]
-            if word in dict:
-                num += str(dict[word])
-                word = ""
-        else:
-            num += str(s[i])
-    return int(num)
+    for key in dict:
+        s = s.replace(key, dict[key])
+    return int(s)
 
 print(solution("oneoneone"))
 
