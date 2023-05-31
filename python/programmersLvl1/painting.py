fed0e65802ddb812ca1b32afc588387d9fdbeab3
u <mjkim2304@gmail.com>
2023-05-31 12:07:18 +0900

    [프로그래머스] 덧칠하기

--- python/programmersLvl1/painting.py
+++ python/programmersLvl1/painting.py
@@ -2,10 +2,20 @@ def solution(n, m, section):
     total = 0
     index = 0
     while index < len(section):
-        for i in range(section[index], section[index] + m):
-            if i in section[index:]:
-                index += 1
+        current = section[index]
+        while index < len(section) and current <= section[index] <= current + m - 1:
+            index += 1
         total += 1
+
     return total
+
+def solution1(n, m, section):
+    answer = 1
+    prev = section[0]
+    for sec in section:
+        if sec - prev >= m:
+            prev = sec
+            answer += 1
+
 
 print(solution(4, 1, [1, 2, 3, 4]))
