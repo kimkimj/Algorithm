3bb013122cf6b2b529b94f1b8a5b2039f928ad2b
u <mjkim2304@gmail.com>
2023-06-07 15:14:10 +0900

    [프로그래머스] 기사단원의 무기 + - lambda

--- python/programmersLvl1/weaponOfKnights.py
+++ python/programmersLvl1/weaponOfKnights.py
@@ -16,5 +16,17 @@ def solution(number, limit, power):
             count = power
         total += count
     print(total)
+
+def commonFactor(n):
+    commonFactors = []
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            commonFactors.append(n // i)
+            commonFactors.append(i)
+        return len(set(commonFactors))
+
+def solution2(number, limit, power):
+    return sum([commonFactor(i) if commonFactor(i) <= limit else power for i in range(1, number + 1)])
+
 solution(10, 3, 2)
 
