032c5cdb026457c68d6cf9404fa00b4087382143
u <mjkim2304@gmail.com>
2023-06-12 11:56:27 +0900

    [백준] 문자열 폭발 *

---
+++ python/DataStructures/Stacks/stringExplosion.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+S = input().strip()
+del_S = list(input().strip())
+l = len(del_S)
+last_word = del_S[-1]
+answer = []
+for i in S:
+    answer.append(i)
+    if i == last_word and answer[-l:] == del_S: # 폭발문자열을 찾은 경우
+        for _ in range(l):
+            answer.pop()
+
+print(''.join(answer)) if answer else print("FRULA")
