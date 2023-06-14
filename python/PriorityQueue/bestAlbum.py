440c46a1dbdcdc8e938de94a5afa0d2791287f23
u <mjkim2304@gmail.com>
2023-06-14 21:33:24 +0900

    [프로그래머스] 베스트 앨범

--- python/PriorityQueue/bestAlbum.py
+++ python/PriorityQueue/bestAlbum.py
@@ -1,6 +1,7 @@
 import heapq
 
 def solution(genres, plays):
+    # genre를 key로 하고, 같은 genre가 나오면 plays 수를 더한다
     dict_genre = {}
     for i in range(len(genres)):
         g = genres[i]
@@ -9,13 +10,15 @@ def solution(genres, plays):
         else:
             dict_genre[g] = dict_genre[g] - plays[i]
 
+    # priority queue에 장르의 총 play 수와 해당 id의 play수를 priority로 저장한다
     pq = []
-
     for i in range(len(plays)):
         g = genres[i]
         p = dict_genre[g]
         heapq.heappush(pq, (p, -plays[i], i))
 
+    # limits 에 genre를 key로, 2번 이상 pq에서 제거가 되었는지를 value로 저장한다
+    # 2번 이하로 저장되었으면 answer에 저장한다
     answer = []
     limits = {}
     for i in range(len(pq)):
