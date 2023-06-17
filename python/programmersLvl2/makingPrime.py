233177f3a9b190055beb080d72141a763a730b69
u <mjkim2304@gmail.com>
2023-06-17 21:38:46 +0900

    [프로그래머스] 소수찾기 (완전탐색) *

--- python/programmersLvl2/makingPrime.py
+++ python/programmersLvl2/makingPrime.py
@@ -1,8 +1,8 @@
 from itertools import permutations
 import math
 
-def isPrime(n):
-    if n == 1:
+def is_prime(n):
+    if n == 0 or n == 1:
         return False
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
@@ -11,23 +11,18 @@ def isPrime(n):
 
 def solution(numbers):
     #l = list(map(int, numbers.split(''))) -> ValueError: empty separator
-    numbers = list(map(int, list(numbers)))
-    count = 0
+    temp = []
     for i in range(1, len(numbers) + 1):
-        perm = permutations([numbers], i)
-        number = ''
-        for combo in list(perm):
-            for c in list(combo):
-                number += c
-        number = int(number)
-        if isPrime(number):
-            count += 1
+        temp += list(permutations(numbers, i))
+
+    unique_numbers = set()
+    count = 0
+    for t in temp:
+        num = int(''.join(t))
+        if num not in unique_numbers:
+            unique_numbers.add(num)
+            if is_prime(num):
+                count += 1
     return count
 
-def solution2(numbers):
-    arr = permutations(numbers)
-    arr = list(arr)
-    for combo in arr:
-        print(combo)
-
-print(solution2(17))
+print(solution("011"))
