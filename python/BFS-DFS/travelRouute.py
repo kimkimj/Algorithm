2a76de5828cd3cc7578f2f7b0dac700d483e5e21
u <mjkim2304@gmail.com>
2023-06-20 11:58:20 +0900

    [프로그래머스] 여행경로 * - backtracking

--- python/BFS-DFS/travelRouute.py
+++ python/BFS-DFS/travelRouute.py
@@ -1,26 +1,32 @@
-def solution(tickets):
-    dict = {}
-    for start, end in tickets:
-        if start in dict:
-            temp = dict[start]
-            temp.append(end)
-            temp.sort(reverse=True)
-            dict[start] = temp
+from collections import deque, defaultdict
+
+def dfs(graph):
+    stack = deque()
+    stack.append("ICN")
+    path = []
+
+    while stack:
+        start = stack.pop()
+        # 더 이상 해당 도시에서 춟발하는 항공권이 없을 때
+        if start not in graph or not graph[start]:
+            path.append(start)
+
         else:
-            dict[start] = [end]
+            # 해당 도시를 다시 stack에 넣고 도착지도 stack에 추가
+            stack.append(start)
+            stack.append(graph[start].pop)
 
-    que = ["ICN"]
-    answer = []
-    while que:
-        current = que.pop() #pop(0) vs pop()
-        answer.append(current)
+    # path를 reverse해서 반환
+    return path[::-1]
 
-        if current in dict and dict[current]:
-            for neighbor in dict[current]:
-                que.append(neighbor)
-            dict[current] = []
-    return answer
+def solution(tickets):
+    graph = defaultdict(list)
+    for start, end in tickets:
+        graph[start].append(end)
 
-tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
-print(solution(tickets))
+    for start in graph.keys():
+        graph[start].sort(reverse = True)
+
+    return dfs(graph)
+
 
