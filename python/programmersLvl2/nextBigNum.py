a1538fccdc6236704cb0981f36ff4ea82949632e
u <mjkim2304@gmail.com>
2023-06-21 11:05:17 +0900

    [프로그래머스] 숫자의 표현 * - bin으로 변환하면 count를 str로 변환없이 사용할 수 있다

---
+++ python/programmersLvl2/nextBigNum.py
@@ -0,0 +1,11 @@
+def solution(n):
+    ones_in_binary = bin(n).count('1')
+    while True:
+        n += 1
+        if ones_in_binary == bin(n).count('1'):
+            return n
+
+print(solution(78))
+
+# str(bin(n)[2:]).count('1')
+# str로 변환할 필요 없이 bin(n).count('1')
