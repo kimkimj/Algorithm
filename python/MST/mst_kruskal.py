c2e0155a8ed9e7bb3422e1a143313373e8ec6df5
u <mjkim2304@gmail.com>
2023-07-03 11:01:31 +0900

    [백준] 최소 스패닝 트리 (Kruskal)

--- python/MST/mst_kruskal.py
+++ python/MST/mst_kruskal.py
@@ -3,46 +3,35 @@ import sys
 input = sys.stdin.readline
 
 v, e = map(int, input().split())
-
-# parent root를 자기 자신으로
 parent = [0] * (v + 1)
 for i in range(1, v + 1):
     parent[i] = i
 
-# 모든 edge와 edge weight 을 저장
+def find_parent(x):
+    while parent[x] != x:
+        x = parent[x]
+    return x
+
+def uninon_parent(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
 edges = []
+total_cost = 0
+
 for _ in range(e):
     a, b, cost = map(int, input().split())
     edges.append((cost, a, b))
 
-# edge weight을 기준으로 오름차순 정렬
 edges.sort()
 
-#def find_root(x):
-# while parent[x] != x:
-#     x = parent[x]
-# return x
-def find_root(parent, x):
-    if parent[x] != x: # ex)
-        parent[x] = find_root(parent, parent[x])
-    return parent[x]
-def union_roots(a, b):
-    root_a = find_root(parent, a)
-    root_b = find_root(parent, b)
+for cost, a, b in edges:
+    if find_parent(a) != find_parent(b):
+        uninon_parent(a, b)
+        total_cost += cost
 
-    if root_a < root_b:
-        parent[b] = root_a
-    else:
-        parent[a] = root_b
-
-total = 0
-# iterate through the edges in the order of edge weight
-for i in range(e):
-    cost, a, b = edges[i]
-
-    # a와 b의 부모가 다르면 union연산 수행해서 mst에 포함
-    if find_root(parent, a) != find_root(parent, b):
-        union_roots(a, b)
-        total += cost
-
-print(total)
+print(total_cost)
