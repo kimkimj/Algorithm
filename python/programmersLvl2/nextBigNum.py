52ec63798c6c0f304c477ef8e52edfafacf1ea0a
u <mjkim2304@gmail.com>
2023-07-10 21:10:17 +0900

    [프로그래머스] 다음 큰 숫자 (복습)

--- python/programmersLvl2/nextBigNum.py
+++ python/programmersLvl2/nextBigNum.py
@@ -5,8 +5,30 @@ def solution(n):
         if ones_in_binary == bin(n).count('1'):
             return n
 
-print(solution(78))
-
-
 # str(bin(n)[2:]).count('1')
+
 # str로 변환할 필요 없이 bin(n).count('1')
+def solution2(n):
+    num_one = bin(n).count('1')
+
+    while True:
+        n += 1
+        if bin(n).count('1') == num_one:
+            return n
+
+print(solution2(78))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
