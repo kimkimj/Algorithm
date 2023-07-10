e249502542ba5ccf7804e2fef9412666d295c3dd
u <mjkim2304@gmail.com>
2023-07-10 19:24:14 +0900

    [프로그래머스] 큰 수 만들기 *

--- python/Greedy/bigNumber.py
+++ python/Greedy/bigNumber.py
@@ -1,23 +1,22 @@
 def solution(number, k):
-    start = 0
-    new_number = ''
-    while k > 0:
-        if start + k >= len(number):
-            start = len(number)
-            k = 0
-        else:
-            slice = number[start: start + k + 1]
-            max_number = max(slice)
-            new_number += max_number
+    answer = []
+    for num in number:
+        while k > 0 and answer and answer[-1] < num:
+            answer.pop()
+            k -= 1
+        answer.append(num)
+    return ''.join(answer)[:len(answer) - k]
 
-            index_max_number_in_slice = slice.find(max_number)
-            k -= index_max_number_in_slice
-            start = start + index_max_number_in_slice + 1
+#9999, 1을 넣으면 9999가 나온다
+number = "9999"
+k = 1
+print(solution(number, k))
 
 
-    if start < len(number) - 1:
-        new_number += number[start:]
 
-    print(new_number)
-solution("4321", 1)
+
+            
+            
+        
+
 
