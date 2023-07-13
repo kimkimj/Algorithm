44bc379cc62a82889d60aa066441b6dc7032299f
u <mjkim2304@gmail.com>
2023-07-14 07:57:29 +0900

    [백준] 랜선 자르기

--- python/BinarySearch/cuttingLANline.py
+++ python/BinarySearch/cuttingLANline.py
@@ -7,16 +7,14 @@ for _ in range(n):
     num = int(input())
     arr.append(num)
 
-start = 0
+# 결과값이 자연수라고 명시했다. 자연수는 1 이상의 모든 정수이다
+start = 1
 end = max(arr)
 result = 1
 
 while start <= end:
     total = 0
     mid = (start + end) // 2
-
-    if mid < 2:
-        mid = 1
 
     for line in arr:
         total += line // mid
