9e860fba39f2a0fcfafb99ca803a53405a27cec3
u <mjkim2304@gmail.com>
2023-07-14 07:15:21 +0900

    이분 탐색 알고리즘

---
+++ python/BinarySearch/BinarySearch.py
@@ -0,0 +1,14 @@
+def binary_search(arr, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] > target:
+            end = mid - 1
+
+        else:
+            start = mid + 1
+
+    return None
