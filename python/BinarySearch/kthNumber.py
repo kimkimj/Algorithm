292d9ab0ba4f3cf771da42b5ace7a94376d7840c
u <mjkim2304@gmail.com>
2023-07-18 00:42:41 +0900

    [백준] k번재 수 *

--- python/BinarySearch/kthNumber.py
+++ python/BinarySearch/kthNumber.py
@@ -1,14 +1,23 @@
 n = int(input())
-k = int(input())
+target = int(input())
 
-arr = [0] * (n * n)
-count = 0
-for row in range(1, n + 1):
-    for column in range(1, n + 1):
-        arr[count] = row * column
-        count += 1
+start = 1
+end = n * n
+answer = 0
 
-arr.sort()
-print(arr[k - 1])
+while start <= end:
+    mid = (start + end) // 2
 
+    count = 0
+    for i in range(1, n + 1):
+        count += min(n, mid // i) #n개가 넘어가면 column의 limit인 n을 초과하는 것이기 때문에
 
+    if count >= target:
+        end = mid - 1
+
+    else:
+        start = mid + 1
+
+print(start)
+
+#https://st-lab.tistory.com/281
