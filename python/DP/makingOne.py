4aaf40716c313d8af322936b0603f5bd8bbb5f0a
u <mjkim2304@gmail.com>
2023-07-31 08:08:43 +0900

    [백준] 1 만들기

--- python/DP/makingOne.py
+++ python/DP/makingOne.py
@@ -1,20 +1,15 @@
 n = int(input())
 
-d = [0] * 3001
+dp = [0] * (n + 1)
 
-# 1은 0
 for i in range(2, n + 1):
-    # 우선 2, 3, 5 아무것도 나누어지지 않는 경우에는 i의 인덱스를 1 줄여줍니다. (횟수는 +1)
-    d[i] = d[i - 1] + 1
-
-    if i % 2 == 0:
-        d[i] = min(d[i], d[i // 2] + 1)
+    dp[i] = dp[i - 1] + 1
 
     if i % 3 == 0:
-        d[i] = min(d[i], d[i // 3] + 1)
+        dp[i] = min(dp[i], dp[i // 3] + 1)
 
-    if i % 5 == 0:
-        d[i] = min(d[i], d[i // 5] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
 
-print(d[n])
+print(dp[n])
 
