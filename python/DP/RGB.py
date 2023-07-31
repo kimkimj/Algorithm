eb7c81df4d86d1f310babf02ed6c35d27e0104f2
u <mjkim2304@gmail.com>
2023-07-31 10:14:48 +0900

    [백준] RGB거리

--- python/DP/RGB.py
+++ python/DP/RGB.py
@@ -3,20 +3,16 @@ cost = []
 for i in range(n):
     cost.append(list(map(int, input().split())))
 
-dp = [[float("inf")] * 3 for _ in range(n)]
-dp[0] = cost[0]
-
-
-for i in range(n - 1):
+for i in range(1, n):
     for j in range(3):
-        if j <= 1:
-            dp[i + 1][2] = min(dp[i + 1][2], dp[i][j] + cost[i + 1][2])
+        if j == 0:
+            cost[i][j] += min(cost[i- 1][1], cost[i- 1][2])
 
-        if j >= 1:
-            dp[i + 1][0] = min(dp[i + 1][0], dp[i][j] + cost[i + 1][0])
+        if j == 1:
+            cost[i][j] += min(cost[i - 1][0], cost[i - 1][2])
 
-        if j == 0 or j == 2:
-            dp[i + 1][1] = min(dp[i + 1][1], dp[i][j] + cost[i + 1][1])
+        if j == 2:
+            cost[i][j] += min(cost[i - 1][0], cost[i - 1][1])
 
-print(min(dp[n - 1]))
+print(min(cost[n - 1]))
 
