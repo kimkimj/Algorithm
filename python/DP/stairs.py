7e1b93593bbab760211debef4e30f97290075eb7
u <mjkim2304@gmail.com>
2023-08-03 19:18:28 +0900

    [백준] 계단 오르기 *

--- python/DP/stairs.py
+++ python/DP/stairs.py
@@ -1,11 +1,14 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 
-d = [0] * len(arr)
-d[0] = arr[0]
-d[1] = arr[0] + arr[1]
-d[2] = max(arr[0] + arr[2], arr[1] + arr[2])
+dp = [0] * n
+dp[0] = arr[0]
+dp[1] = dp[0] + arr[1]
+# current stair MUST be incldued (max on the premise that the current stiar is being stepped on)
+dp[2] = max(arr[0] + arr[2], arr[1] + arr[2])
 
-for i in range(3, len(arr)):
-    d[i] = max(d[i - 3] + arr[i - 1] + arr[i], d[i - 2] + arr[i])
-print(d[n - 1])
+for i in range(3, n):
+    dp[i] = arr[i] + max(dp[i - 3] + arr[i - 1], dp[i - 2])
+
+print(dp[n - 1])
+
