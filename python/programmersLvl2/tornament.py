72d2ec66796e9f5486526427715503d9f1908c53
u <mjkim2304@gmail.com>
2023-08-08 12:01:27 +0900

    [프로그래머스] 예상 대진표 *

--- python/programmersLvl2/tornament.py
+++ python/programmersLvl2/tornament.py
@@ -1,21 +1,11 @@
 import math
+
 def solution(n,a,b):
-
-    if a <= n // 2 and b > n //2 or b <= n// 2 and a > n// 2:
-
-        return int(math.log(n, 2))
-
-    else:
-        group_a = int(a / 2)
-        group_b = int(b / 2)
-
-        #return int(math.log(abs(group_a - group_b), 2)) + 1
-        prev = int(math.log(n, 2)) - 1
-        prev = pow(2, prev)
-        return int(math.log(prev, 2))
-
-n = 16
-a = 1
-b = 7
-print(solution(n, a, b))
-
+    answer = 0
+    while True:
+        a = math.ceil(a / 2)
+        b = math.ceil(b / 2)
+        answer += 1
+        if a == b:
+            break
+    return answer
