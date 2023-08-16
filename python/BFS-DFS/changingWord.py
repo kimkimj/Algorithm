38c703cb6868c1e1ac119263c3727389f9ac3d51
u <mjkim2304@gmail.com>
2023-08-16 12:05:49 +0900

    [프로그래머스] 단어 변환 (bfs) +

--- python/BFS-DFS/changingWord.py
+++ python/BFS-DFS/changingWord.py
@@ -1,48 +1,29 @@
-def one_letter_diff(current, comparison):
-    diff_count = 0
-    for k in range(len(current)):
-        if current[k] != comparison[k]:
-            diff_count += 1
-    if diff_count == 1:
-        return True
-    return False
+from collections import deque
 
 def solution(begin, target, words):
-    graph = {}
-    graph[begin] = []
-    for word in words:
-        graph[word] = []
-        if one_letter_diff(begin, word):
-            graph[begin].append(word)
 
-    for i in range(len(words)):
-        current = words[i]
-        for j in range(i + 1, len(words)):
-            comparison = words[j]
+    que = deque([(begin, 0)])
+    visited = set()
+    while que:
+        current_word, count = que.popleft()
 
-            if one_letter_diff(current, comparison):
-                graph[current].append(comparison)
-                graph[comparison].append(current)
+        if current_word == target:
+            return count
 
-    answer = float("INF")
-    stack = [(begin, set(), 0)]
-    while stack:
-        current, visited, count = stack.pop()
-        visited.add(current)
-        if current == target:
-            answer = min(answer, count)
-            break
+        for word in words:
+            letter_diff_count = 0
+            for i in range(len(word)):
+                if word[i] != current_word[i]:
+                    letter_diff_count += 1
+            if letter_diff_count == 1:
+                if word not in visited:
+                    que.append((word, count + 1))
+                    visited.add(word)
+    return 0
 
-        for changeable in graph[current]:
-            if changeable not in visited:
-                stack.append((changeable, visited, count + 1))
 
-    if answer == float("INF"):
-        print(0)
-    else:
-        print(answer)
 
 begin = "hit"
 target = "cog"
 words = ["hot", "dot", "dog", "lot", "log"]
-solution(begin, target, words)
+print(solution(begin, target, words))
