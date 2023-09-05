58637c5527e0090f00df5c58b14fc71f543b65e1
u <mjkim2304@gmail.com>
2023-09-05 12:11:41 +0900

    [이코테] 떡볶이 떡 만들기 (복습)

--- python/BinarySearch/makingTtekbooki.py
+++ python/BinarySearch/makingTtekbooki.py
@@ -2,23 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n, min_height = map(int, input().split())
-arr = list(map(int, input().split()))
+heights = list(map(int, input().split()))
 
 start = 0
-end = max(arr)
+end = max(heights)
 result = 0
-
 while start <= end:
+    total = 0
     mid = (start + end) // 2
 
-    total = 0
-    for h in arr:
-        total += max(0, h - mid)
+    for t in heights:
+        if t - mid > 0:
+            total += t - mid
 
-    if total >= min_height:
+    # 절단기의 높이를 늘리면 더 적은 양의 떡이 남고, 낮추면 더 많이 남는다
+    # 자른 떡의 양이 min_height이 되지 않는다면 절단기의 높이를 낮춰야 한다 (decrease mid)
+    if total < min_height:
+        end = mid - 1
+    else: # 자른 떡의 양이 min_height과 같거나 그보다 클 때는 절단기의 높이를 늘려야 한다(increase mid)
         result = mid
         start = mid + 1
 
-    else:
-        end = mid - 1
-print(result)
+
+
+print(total)
