5235cd141bc887367d035e980ca8debf41bb0b26
u <mjkim2304@gmail.com>
2023-09-06 11:49:43 +0900

    [백준] 랜선 자르기 (복습) - 자연수는 1 이상의 모든 정수이다

--- python/BinarySearch/cuttingLANline.py
+++ python/BinarySearch/cuttingLANline.py
@@ -1,29 +1,33 @@
 import sys
+
 input = sys.stdin.readline
 
-n, req = map(int, input().split())
-arr = []
-for _ in range(n):
-    num = int(input())
-    arr.append(num)
+k, n = map(int, input().split())
+cables = []
+for _ in range(k):
+    cables.append(int(input()))
 
 # 결과값이 자연수라고 명시했다. 자연수는 1 이상의 모든 정수이다
+# 1로 하지 않으면 division by zero가 나오는 케이스가 있다
 start = 1
-end = max(arr)
-result = 1
-
+end = max(cables)
+result = 0
 while start <= end:
-    total = 0
     mid = (start + end) // 2
+    if mid == 0:
+        break
 
-    for line in arr:
+    total = 0
+    for line in cables:
         total += line // mid
 
-    if total >= req:
-        start = mid + 1
-        result = mid
+    if total < n:
+        end = mid - 1
 
     else:
-        end = mid -1
+        result = mid
+        start = mid + 1
 
 print(result)
+
+
