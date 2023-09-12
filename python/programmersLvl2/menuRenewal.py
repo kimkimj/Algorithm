e12125c7081ef04990f29ca68f1b4db28f4853ba
u <mjkim2304@gmail.com>
2023-09-12 12:07:08 +0900

    [프로그래머스] 2021 KBR 메뉴 리뉴얼 *

--- python/programmersLvl2/menuRenewal.py
+++ python/programmersLvl2/menuRenewal.py
@@ -1,41 +1,16 @@
 from itertools import combinations
-from collections import defaultdict
+from collections import Counter
+
 
 def solution(orders, course):
-    menus = set()
-    for o in orders:
-        for single in o:
-            menus.add(single)
-    # put every single menu in the set
-
     answer = []
-    for num_menu in course:
-        customers = defaultdict(list)
-        largest = 0
-        for c in combinations(menus, num_menu):
-            combo = ''.join(list(c))
-            count = 0
-            for o in orders:
-                letters = 0
-                for c in o:
-                    if c in combo:
-                        letters += 1
-                if letters == len(combo):
-                    count += 1
+    for c in course:
+        temp = []
+        for order in orders:
+            combi = combinations(sorted(order), c)
+            temp += combi
+        counter = Counter(temp)
+        if len(counter) != 0 and max(counter.values()) != 1:
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
 
-            if count > largest:
-                largest = count
-
-            if count >= 2:
-                customers[count].append(combo)
-
-        for c in customers:
-            print(c)
-            if c == largest:
-                for combo in customers[c]:
-                    answer.append(''.join(sorted(combo)))
     return sorted(answer)
-
-orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
-course = [2,3,4]
-print(solution(orders, course))
