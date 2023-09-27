2fed00e50e48c78df7b18cb50eb2516439a8acf2
u <mjkim2304@gmail.com>
2023-09-27 21:47:11 +0900

    [백준] n과 m (3)

---
+++ python/Backtracking/nAndM3.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+comb = []
+
+def recur():
+    if len(comb) == m:
+        print(*comb)
+        return
+
+    for i in range(1, n + 1):
+        comb.append(i)
+        recur()
+        comb.pop()
+
+recur()
