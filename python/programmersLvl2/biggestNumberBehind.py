c226107c4059c291e216351f80c30c2be18fdb70
u <mjkim2304@gmail.com>
2023-10-09 06:17:48 +0900

    [백준]가장 긴 증가하는 부분 수열

--- python/programmersLvl2/biggestNumberBehind.py
+++ python/programmersLvl2/biggestNumberBehind.py
@@ -5,12 +5,11 @@ def solution(numbers):
     for i in range(len(numbers)):
         current = numbers[i]
         to_be_removed = []
-        for index in stack:
+        while stack and stack[-1]
             if current > numbers[index]:
                 ans[index] = current
                 to_be_removed.append(index)
-        for n in to_be_removed:
-            stack.remove(n)
+
 
         stack.append(i)
 
@@ -19,7 +18,3 @@ def solution(numbers):
 numbers = [2, 3, 3, 5]
 print(solution(numbers))
 
-# for each loop는 사실 index에 기반으로 돌아가기 때문에,
-# for index in stack을 했을 때, 즉 for each loop을 도는 객체의
-# 사이즈가 줄면 for loop도 조기 종영될 수 있다...
-# index가 1이였는데 stack의 사이즈가 1임..
