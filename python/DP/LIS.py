5ab562f5671720da21809e0b5a4cb9f5126105bf
u <mjkim2304@gmail.com>
2023-10-09 05:26:26 +0900

    [백준]가장 긴 증가하는 부분 수열

--- python/DP/LIS.py
+++ python/DP/LIS.py
@@ -1,22 +1,14 @@
 n = int(input())
+
 arr = list(map(int, input().split()))
-dp = [1] * n
-for i in range(len(arr)):
+dp = []
+dp.append(1)
+
+for i in range(1, len(arr)):
+    lcs = 1
     for j in range(i):
-        if arr[i] > arr[j]:
-            dp[i] = max(dp[j] + 1, dp[i])
-length = max(dp)
-print(length)
+        if arr[j] < arr[i]:
+            lcs = max(lcs, dp[j] + 1)
+    dp.append(lcs)
 
-subsequence = []
-order = length
-for i in range(n - 1, -1, -1):
-    if dp[i] == order:
-        subsequence.append(arr[i])
-        order -= 1
-
-subsequence.reverse()
-print(*subsequence)
-
-# 8
-# 10 20 10 10 40 20 30 40
+print(max(dp))
